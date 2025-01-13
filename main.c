a430526f8bac01ad55dba3cfe2992fb2793e99b8
mazilimka <tortik8747@gmail.com>
2025-01-13 21:57:19 +0300

    A little optimization update

--- main.c
+++ main.c
@@ -2,34 +2,45 @@
 #include <stdio.h>
 #include <stdbool.h>
 
+void insertParenthesis();
 
-int main() {
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd) {
     INPUT input = {0};
     input.type = INPUT_KEYBOARD;
     while (1) {
-        // Проверяем состояние клавиш '9' и 'Shift'
         if ((GetAsyncKeyState(VK_SHIFT) & 0x8000) && (GetAsyncKeyState('9') & 0x8000)) {
-            break;
+            insertParenthesis();
         }
-        Sleep(80);
+        Sleep(100);
     }
+    return 0;
+}
 
-    //Отправляем нажатие '0' (закрывающая скобка)
-    input.ki.wVk = '0';
-    input.ki.dwFlags = 0; 
-    SendInput(1, &input, sizeof(INPUT));
-    Sleep(100);
+void insertParenthesis() {
+    INPUT input = {0};
+    input.type = INPUT_KEYBOARD;
 
-    // Отпускаем '0'
-    input.ki.dwFlags = KEYEVENTF_KEYUP; 
+    input.ki.wVk = VK_SHIFT;
+    input.ki.dwFlags = 0;
     SendInput(1, &input, sizeof(INPUT));
     Sleep(50);
 
-    // Перемещаем курсор влево
+    input.ki.wVk = '0';
+    input.ki.dwFlags = 0; 
+    SendInput(1, &input, sizeof(INPUT));
+    Sleep(50);
+
+    input.ki.wVk = VK_SHIFT;
+    input.ki.dwFlags = KEYEVENTF_KEYUP;
+    SendInput(1, &input, sizeof(INPUT));
+    Sleep(50);
+
+    input.ki.wVk = '0';
+    input.ki.dwFlags = KEYEVENTF_KEYUP; 
+    SendInput(1, &input, sizeof(INPUT));
+    // Sleep(50);
+
     input.ki.wVk = VK_LEFT; 
     input.ki.dwFlags = 0; 
     SendInput(1, &input, sizeof(INPUT));
-    Sleep(100);
-    
-    main();
 }
